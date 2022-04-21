79882e7a5a26b356010a1548cdbf7979e8a9bc67
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-04-21 15:54:50 +0100

    getting data from data base

---
+++ aspmvc-learn/Controllers/ItemController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using aspmvc_learn.Data;
+using aspmvc_learn.Models;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace aspmvc_learn.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;
+                 
+            return View(objList);
+        }
+    }
+}
